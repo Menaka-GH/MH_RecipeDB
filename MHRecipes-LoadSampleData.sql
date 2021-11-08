a0643392f596fc8fa809b7a7c40742d71adff437
Menaka-GH <menakasuthankk@gmail.com>
2021-11-07 22:12:42 -0500

    Loaded data into the tables

---
+++ MHRecipes-LoadSampleData.sql
@@ -0,0 +1,28 @@
+INSERT INTO Recipe_Category(category_name) VALUES('Dinner Recipes')
+INSERT INTO Recipe_Category(category_name) VALUES('Breakfast Recipes');
+INSERT INTO Recipe_Category(category_name) VALUES('Rice Recipes');
+INSERT INTO Recipe_Category(category_name) VALUES('Snack Recipes');
+INSERT INTO Recipe_Category(category_name) VALUES('Sweet Recipes');
+INSERT INTO Recipe_Category(category_name) VALUES('Chutney Recipes');
+INSERT INTO Recipe_Category(category_name) VALUES('Curry Recipes');
+
+
+---Inserting values for the table Recipes
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sambar','Curry recipe mixed with Toor dal,veggies and spices',7);
+INSERT INTO Recipes values('Panneer Masala','Panneer mixed with tomato, cashew gravy and spices',7);
+INSERT INTO Recipes values('Vegetables Curry','Veggies mixed with spices and coconut gravy',7);
+INSERT INTO Recipes values('Mysore Pak','Sweet recipe made with Besan, sugar and ghee',5);
+INSERT INTO Recipes values('Peanut Laddoo','Crushed peanuts mixed with Jaggery syrup and nuts',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
+INSERT INTO Recipes values('Sakkarai Pongal','Sweet recipe made with Rice,Moong dal and Jaggery',5);
