eafe7071465013bb2e2cd650a4bf2fd13d5405bd
Menaka-GH <menakasuthankk@gmail.com>
2021-11-17 21:48:25 -0500

    Files updated

--- ExecuteRecipes.sql
+++ ExecuteRecipes.sql
@@ -1,22 +1,48 @@
 --To update the recipes
-EXECUTE UpdateRecipe "Urad Dal Vada","Urad dal Fritters";
+
+EXECUTE UpdateRecipe "Sakkarai Pongal","SakkaraiPongal";
+
 ---input for the stored procedure CreateIncredients
+
 --To know the incredientid of the inserted row
+
 DECLARE @IncredId INT
-
-
-EXEC CreateIncredients 'salt', @IncredId OUTPUT
-
+EXEC CreateIncredients 'Garammasala powder', @IncredId OUTPUT
 SELECT @IncredId Incredients_Id
 
----To get the count of users for the recipe
+select *from Incredients;
+
+--to list the users
+
+select *from Users;
+
+---To get the count of users commented for the recipe
+
 SELECT dbo.NumberOfUsers(1); 
+
 ---TO GET THE NUMBER OF CATEGORIES
+
 EXEC NumberOfCategories;
 
+--To list the comments for the recipes
+
+select *from Comments;
+
 --To get the comments on recipes by date
-select * from CommentsOnPDate('11/10/2020');
+
+select * from CommentsOnPDate('2020-12-20');
+
 --To list the recipes
+
 EXECUTE ReadRecipes;
+
+--to delete the recipe
+
+EXECUTE DeleteRecipe 'Bajji';
+
 --To list the recipes and category
+
 EXEC ListRecipeCategory
+
+select *from Recipe_Category;
+select *from Recipes;
--- MHRecipes-CreateDBObjects.sql
+++ MHRecipes-CreateDBObjects.sql
@@ -1,23 +1,13 @@
 
+IF EXISTS ( SELECT [name] FROM sys.databases WHERE [name] = 'MH_RECIPES' )
+DROP DATABASE MH_RECIPES
+GO
+
 CREATE DATABASE MH_RECIPES;
+GO
+
 USE MH_RECIPES;
-IF OBJECT_ID('Recipes', 'U') IS NOT NULL
-    DROP TABLE Recipes;
-CREATE TABLE Recipes
-( recipe_id INT NOT NULL IDENTITY PRIMARY KEY,
-  recipe_name VARCHAR(255) NOT NULL,
-  recipe_description VARCHAR(255),
-  recipe_category_id INT NOT NULL
-  FOREIGN KEY(recipe_category_id) REFERENCES Recipe_Category(recipe_category_id));
-  GO
-  IF OBJECT_ID('Recipe_Category', 'U') IS NOT NULL
-    DROP TABLE Recipe_Category;
-CREATE TABLE Recipe_Category
-( recipe_category_id INT NOT NULL IDENTITY PRIMARY KEY,
-  category_name VARCHAR(255) NOT NULL,
-  );
-  GO
---  Recipe_steps-steps_id-recipe_id-instructions-prep_time-cook_time
+
 IF OBJECT_ID('Recipe_steps', 'U') IS NOT NULL
     DROP TABLE Recipe_steps;
 CREATE TABLE Recipe_steps
@@ -28,14 +18,22 @@ CREATE TABLE Recipe_steps
   cook_time INT,
   FOREIGN KEY(recipe_id) REFERENCES Recipes(recipe_id));
   GO
---Incredients	-ingredient_id(primary key)-ingredient_name	
-IF OBJECT_ID('Incredients', 'U') IS NOT NULL
-    DROP TABLE Incredients;
-CREATE TABLE Incredients
-( incredient_id INT NOT NULL IDENTITY PRIMARY KEY,
-  incredient_name varchar(250) NOT NULL,
-  );
+
+ --comments:user_id, recipe_id, comment,posted_date
+IF OBJECT_ID('Comments', 'U') IS NOT NULL
+    DROP TABLE Comments;
+CREATE TABLE Comments
+ (comments_id INT NOT NULL IDENTITY PRIMARY KEY,
+  userid INT NOT NULL,
+  recipe_id INT NOT NULL,
+  comment text,
+  posted_date date,
+  FOREIGN KEY(userid) REFERENCES Users(userid),
+  FOREIGN KEY(recipe_id) REFERENCES Recipes(recipe_id));
   GO
+
+
+
 ---Recipe_incredients:id,incredient_id,recipe_id,amount_required
 IF OBJECT_ID('Recipe_Incredients', 'U') IS NOT NULL
     DROP TABLE Recipe_Incredients;
@@ -46,29 +44,46 @@ CREATE TABLE Recipe_Incredients
   amount_required varchar(50),
   FOREIGN KEY(incredient_id) REFERENCES Incredients(incredient_id),
   FOREIGN KEY(recipe_id) REFERENCES Recipes(recipe_id));
-   
   GO
-  --Users:user_id,user_email
-  IF OBJECT_ID('Users', 'U') IS NOT NULL
+
+IF OBJECT_ID('Recipes', 'U') IS NOT NULL
+    DROP TABLE Recipes;
+CREATE TABLE Recipes
+( recipe_id INT NOT NULL IDENTITY PRIMARY KEY,
+  recipe_name VARCHAR(255) NOT NULL,
+  recipe_description VARCHAR(255),
+  recipe_category_id INT NOT NULL
+  FOREIGN KEY(recipe_category_id) REFERENCES Recipe_Category(recipe_category_id));
+  GO
+
+
+IF OBJECT_ID('Recipe_Category', 'U') IS NOT NULL
+    DROP TABLE Recipe_Category;
+CREATE TABLE Recipe_Category
+( recipe_category_id INT NOT NULL IDENTITY PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL,
+  );
+  GO
+--  Recipe_steps-steps_id-recipe_id-instructions-prep_time-cook_time
+
+--Incredients	-ingredient_id(primary key)-ingredient_name	
+IF OBJECT_ID('Incredients', 'U') IS NOT NULL
+    DROP TABLE Incredients;
+CREATE TABLE Incredients
+( incredient_id INT NOT NULL IDENTITY PRIMARY KEY,
+  incredient_name varchar(250) NOT NULL,
+  );
+  GO
+
+--Users:user_id,user_email
+IF OBJECT_ID('Users', 'U') IS NOT NULL
     DROP TABLE Users;
 CREATE TABLE Users
 (userid INT NOT NULL IDENTITY PRIMARY KEY,
   user_email varchar(255) NOT NULL,
   );
   Go 
- --comments:user_id, recipe_id, comment,posted_date
-   IF OBJECT_ID('Comments', 'U') IS NOT NULL
-    DROP TABLE Comments;
-CREATE TABLE Comments
-(comments_id INT NOT NULL IDENTITY PRIMARY KEY,
-  userid INT NOT NULL,
-  recipe_id INT NOT NULL,
-  comment text,
-  posted_date date,
-  FOREIGN KEY(userid) REFERENCES Users(userid),
-  FOREIGN KEY(recipe_id) REFERENCES Recipes(recipe_id));
-   
-  GO
+
 
  ---indexes
 
@@ -78,16 +93,7 @@ GO
 
 CREATE NONCLUSTERED INDEX IX_CategoryName ON Recipe_Category (category_name DESC)
 GO
---stored procedure
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'ReadRecipeCategories' AND ROUTINE_TYPE = N'PROCEDURE')
-DROP PROCEDURE ReadRecipeCategories
-GO
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'DeleteRecipe' AND ROUTINE_TYPE = N'PROCEDURE')
-DROP PROCEDURE DeleteRecipe
-GO
-IF EXISTS ( SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'UpdateRecipe' AND ROUTINE_TYPE = N'PROCEDURE')
-DROP PROCEDURE UpdateRecipe
-GO
+
 
 --stored procedure
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'CreateIncredients' AND ROUTINE_TYPE = N'PROCEDURE')
@@ -99,18 +105,17 @@ CREATE PROCEDURE CreateIncredients
 @IncredId int output)
 
 AS
-
 BEGIN
-
 SET NOCOUNT ON;
-
 INSERT INTO Incredients (incredient_name) VALUES (@Incredname)
-
 SELECT @IncredId= SCOPE_IDENTITY()
-
 END;
-go
+GO
 --stored procedure: ReadRecipeCategories
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'ReadRecipeCategories' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE ReadRecipeCategories
+GO
+
 CREATE PROCEDURE ReadRecipeCategories(@rname as varchar(255))
 AS 
 BEGIN
@@ -122,7 +127,11 @@ where R.recipe_name like '%'+ @rname
 END;
 
 GO
-------------------------------------------------
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'DeleteRecipe' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE DeleteRecipe
+GO
+
 --delete stored procedure
 CREATE PROCEDURE DeleteRecipe @RecipeName NVARCHAR(255) AS
 BEGIN
@@ -138,6 +147,10 @@ END
 GO
 
 ---updaterecipe
+IF EXISTS ( SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'UpdateRecipe' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE UpdateRecipe
+GO
+
 CREATE PROCEDURE UpdateRecipe @OldRecipeName NVARCHAR(255), @NewRecipeName NVARCHAR(255) AS
 BEGIN
     SET NOCOUNT ON;
@@ -160,6 +173,12 @@ ORDER BY
 	rc.category_name
 END;
 GO
+
+IF object_id('NumberOfUsers', 'FN') IS NOT NULL
+BEGIN
+    DROP FUNCTION [dbo].[NumberOfUsers]
+END
+GO
 ---NumberOfUser function
 CREATE FUNCTION NumberOfUsers(@RecipeID INT)
 RETURNS INT
@@ -170,6 +189,13 @@ BEGIN
 END;
 GO
 --Table valued function
+
+IF object_id('CommentsOnPDate', 'FN') IS NOT NULL
+BEGIN
+    DROP FUNCTION [dbo].[CommentsOnPDate]
+END
+GO
+
 CREATE FUNCTION CommentsOnPDate (
     @PostedDate Date) 
 RETURNS TABLE
@@ -202,7 +228,8 @@ DROP PROCEDURE ListRecipeCategory
 GO
 CREATE OR ALTER PROCEDURE ListRecipeCategory AS
 BEGIN
-	SELECT	r.recipe_name,rc.category_name
+	SELECT	r.recipe_name,
+	rc.category_name
 FROM
 	Recipes r
 INNER JOIN Recipe_Category rc ON r.recipe_category_id = rc.recipe_category_id
